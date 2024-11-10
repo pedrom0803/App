5bff2ac294f0680c599d7c390a042054c64017ec
pedrom0803 <pedro.m.miguel.2003@gmail.com>
2024-11-10 17:59:27 +0000

    main client page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ClientHomePage from "./pages/ClientHomePage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<ClientHomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+    screens: {
+      quatro: "400px",
+      cinco: "500px",
+      seis: "600px",
+      oito: "800px",
+      mil: "1000px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
