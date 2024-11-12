f655f5dffd59a8edfa5ff32c0a85beb828ab82f5
pedrom0803 <pedro.m.miguel.2003@gmail.com>
2024-11-12 12:33:16 +0000

    inicio base dados

--- backend/back/models.py
+++ backend/back/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Utilizador(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField()
+    morada = models.CharField(null=True)
+    contacto = models.CharField(max_length=9,null=True)
+    codigo_postal= models.CharField(max_length=8,null=True)
+    distrito  = models.CharField(null=True,choices=Distritos)
+    
+    
